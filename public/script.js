4e76841aa373f25ee3b3acd596399d23ca7f289b
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2021-06-20 18:27:51 +0530

    test mutes

--- public/script.js
+++ public/script.js
@@ -2,16 +2,18 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer()
 const myVideo = document.createElement('video')
+myVideo.muted = true;
 const peers = {}
 let myStream;
 
 var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 getUserMedia({video: true, audio: true}, function(stream) {
+  myStream = stream;
+
   addVideoStream(myVideo, stream)
 
   myPeer.on('call', call => {
     // getUserMedia({video: true, audio: true}, function(stream) {
-      myStream = stream;
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
