62f752197c387b05691692e4aa37432b6e1adbe2
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2021-06-20 17:07:07 +0530

    remove mute

--- public/script.js
+++ public/script.js
@@ -2,7 +2,6 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer()
 const myVideo = document.createElement('video')
-myVideo.muted = true
 const peers = {}
 let myStream;
 
